31ad7ff73abe4ca12300f5d58cf748c1d49e1e1d
Bikmay <artyom.ivliev@gmail.com>
2017-10-16 00:34:40 +0400

    Добавьте файлы проекта.

---
+++ CheckListFronts/WorkForList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Drawing.Text;
+
+namespace CheckListFronts
+{
+    class WorkForList
+    {
+
+        public static int countOfFamiliesFonts = 0;
+
+        public static void Getway()
+        {
+            FontFamily[] fontFamilies; //Создаём массив семей шрифтов
+
+
+            InstalledFontCollection installedFontCollection = new InstalledFontCollection();//Создаём и инициализруем объект штоб воткнуть потом в массив
+
+            fontFamilies = installedFontCollection.Families; //впихиваем массив семьи щрифтов
+
+            countOfFamiliesFonts = fontFamilies.Length; // на всякий берём их колличество
+
+            
+
+        }
+
+    }
+}
